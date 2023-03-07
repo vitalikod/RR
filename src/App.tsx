9e3adf920988260639fc1dd4491d804c4ff39bf0
vitalikod <vitalikod879@gmail.com>
2023-03-07 20:05:51 +0200

    qqq

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './components/home/home';
-import Login from './components/auth/login';
 import PrivateRoute from './components/utils/router/privateRoute';
+import AuthRootComponent from './components/auth';
 
 
-function App(): JSX.Element {
+function App() {
   return (
     <div>
       <Routes>
         <Route element={<PrivateRoute />}>
           <Route path='/' element={<Home />} />
         </Route>
-        <Route path='login' element={<Login />} />
+        <Route path='login' element={<AuthRootComponent />} />
+        <Route path='register' element={<AuthRootComponent />} />
       </Routes>
     </div>
   );
